8b87b0de3c77dce27cb435839a934118d8afe39c
Rajath K Naik <rajathkrishnamurthy173@gmail.com>
2024-11-16 19:52:49 +0530

    Update g16.c

--- g16.c
+++ g16.c
@@ -18,8 +18,8 @@ int main()
 {
  int n;
  scanf("%d",&n);
- int rem1=n%10;
- int q=n/10;    //231 ans 4
- int rem2=q%10;
+ int rem1=n%10;        //last dighit
+ int q=n/10;    //231 ans 4         // divide a number by 10 until it becomes 0
+ int rem2=q%10;               //last 2nd digit
  printf("%d",rem1+rem2);
 }
